de19c96712926475682e4ebcca3efb7968767656
Abraham Kinney <abraham.kinney@lootcrate.com>
2017-10-10 21:40:13 -0700

    update endpoint added

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,20 @@
 module Api::V1
   class CommentsController < ApplicationController
+
     def index
       @comments = Comment.all
       render json: @comments
     end
+
+    def create
+      @comment = Comment.create(comment_params)
+      render json: @comment
+    end
+
+    private
+
+      def comment_params
+        params.require(:comment).permit(:rating, :comment_body)
+      end
   end
 end
