d9a5d024f8e319eadb9486d92a8e333d91381830
Abraham Kinney <abraham.kinney@lootcrate.com>
2017-10-15 17:06:20 -0700

    started doctor suggestions funciton

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -7,8 +7,13 @@ module Api::V1
     end
 
     def create
-      @comment = Comment.create(comment_params)
-      render json: @comment
+      comment = Comment.create(comment_params)
+       if comment.save
+          suggest_doctors(Doctor.find(comment.doctor_id), comment.rating)
+          render json: comment, suggest_doctors
+       else
+          render json: @comment.errors, status: :unprocessable_entity
+       end
     end
 
     def update
@@ -17,21 +22,26 @@ module Api::V1
       render json: @comment
     end
 
-    #
-    # comment = Comment.new(comment_params)
-    #  if comment.save
-    #     render json: comment
-    #  else
-    #     render whatever
-    #  end
-    #
+
+    def suggest_doctors(doctor, rating)
+      Doctor.within(5, :origin => [doctor.lng, doctor.lat]).count #withing 5
+      # same specialty_id
+      # average rating that is equal to or higher than passed rating
+
+      ### IN THIS VERSION THE RATING IS PASSED BUT ONCE A AUTHENTICATED USER IS AVAILABLE
+      ### WE WOULD WANT TO DECOUPLE THIS TO LOOK UP WHAT THE CURRENT USER RATED THE DOC AT
+      ### THIS WAY WE COULD USE THIS FUNCTION IN OTHER AREAS AND LOOK IT UP. WE WOULD
+      ### WANT MAKE THE FUNCTION SMARTER SO IT WOULD RETURN SUGGESTIONS BASED ON OTHER THINGS
+      ## RATHER THAN SIMPLY BEING TIED TO A COMMENT CREATION
+
+      return suggested_doctors
+    end
+
     # def disable
     #   @comment = Comment.find(params[:id])
     #   @comment.update_attributes(comment_params)
     #   render json: @comment
     # end
-
-
 
     def destroy
       @comment = Comment.find(params[:id])
